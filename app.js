f72ec340fa719c3abd2179cd5c21602a28ee69c4
gitmwon <129269229+gitmwon@users.noreply.github.com>
2023-11-12 12:28:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static("./public"));
 
-const ExpressServer = app.listen(PORT, () => {
+const ExpressServer = app.listen(PORT,"0.0.0.0", () => {
   console.log(`listening on port ${PORT}`);
 });
 
